39f640f87a6e9428cb395ba1490bde3af1dd2c19
Bacale-Kevin <bacale86@gmail.com>
2023-07-20 00:55:47 +0100

    feat: added paging, sorting, filtering with the help of the libary Dynamic Linq

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using MyBGList.DataContext;
 using MyBGList.DTO;
 using MyBGList.Models;
+using System.Linq.Dynamic.Core;
 
 namespace MyBGList.Controllers
 {
@@ -22,21 +23,30 @@ namespace MyBGList.Controllers
 
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)] // Caching this route for 1minute
-        public async Task<RestDTO<BoardGame[]>> Get()
+        public async Task<RestDTO<BoardGame[]>> Get(int pageNumber = 0, int pageSize = 10, string? sortColumn = "Name", string? sortOrder = "ASC", string? filterQuery = null)
         {
 
-            var query = _context.BoardGames;
+            var query = _context.BoardGames.AsQueryable();
+            if (!string.IsNullOrEmpty(filterQuery))
+                query = query.Where(b => b.Name.Contains(filterQuery));
+            var recordCount = await query.CountAsync();
+            query = query
+            .OrderBy($"{sortColumn} {sortOrder}")
+            .Skip(pageNumber * pageSize)
+            .Take(pageSize);
+
             return new RestDTO<BoardGame[]>()
             {
                 Data = await query.ToArrayAsync(),
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                RecordCount = recordCount,
                 Links = new List<LinkDTO> {
-                    new LinkDTO(
-                                Url.Action(null, "BoardGames", null, Request.Scheme)!, // Returns the URL endpoint
-                                "self",
-                                "GET"
-                                ),
-                }
-
+                                new LinkDTO(Url.Action(null, "BoardGames", new { pageNumber, pageSize }, Request.Scheme)!,
+                                            "self",
+                                             "GET"
+                                               ),
+}
             };
         }
 
